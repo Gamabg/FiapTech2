265cff3b96eb5952eefb198bc9478b8520df4cf8
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-24 09:28:42 -0300

    Finalizado

--- fiaptech.js
+++ fiaptech.js
@@ -2,44 +2,43 @@ const mongoose = require("mongoose");
 const express = require("express");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
-const path = require('path');
+const path = require("path");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 const port = 3000;
-app.use(express.static("public"))
+app.use(express.static("public"));
 
-mongoose.connect('mongodb://127.0.0.1:27017/humansunity', {
+mongoose.connect("mongodb://127.0.0.1:27017/FiapTech", {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
-mongoose.connection.on('error', err => {
-  console.error('Erro de conexão com o MongoDB:', err);
+mongoose.connection.on("error", (err) => {
+  console.error("Erro de conexão com o MongoDB:", err);
 });
 
 const usuarioSchema = new mongoose.Schema({
   nome: { type: String },
   email: { type: String, required: true },
-  senha: { type: String }
+  senha: { type: String },
 });
 
 const Usuario = mongoose.model("Usuario", usuarioSchema);
 
 const produtoSchema = new mongoose.Schema({
-  produto: { type: String },
-  codigo: { type: String },
+  codigo: { type: String, Required: true },
   descricao: { type: String },
   fornecedor: { type: String },
   dataFabricacao: { type: Date },
-  quantidadeEstoque: { type: Number }
+  quantidadeEstoque: { type: Number },
 });
 
 const Produto = mongoose.model("Produto", produtoSchema);
 
-app.post("/cadastro-usuario", async (req, res) => {
+app.post("/cadastrousuario", async (req, res) => {
   const { nome, email, senha } = req.body;
 
   const hashedPassword = await bcrypt.hash(senha, 10);
@@ -58,33 +57,32 @@ app.post("/cadastro-usuario", async (req, res) => {
   }
 });
 
-app.post("/cadastro-produto", async (req, res) => {
-  const { codigo, descricao, fornecedor, dataFabricacao, quantidadeEstoque } = req.body;
+app.post("/cadastroproduto", async (req, res) => {
+  const { codigo, descricao, fornecedor, dataFabricacao, quantidadeEstoque } =
+    req.body;
 
   const produto = new Produto({
-    produto: produto,
     codigo: codigo,
     descricao: descricao,
     fornecedor: fornecedor,
     dataFabricacao: dataFabricacao,
-    quantidadeEstoque: quantidadeEstoque
+    quantidadeEstoque: quantidadeEstoque,
   });
 
   try {
     const newProduto = await produto.save();
-    res.json({ error: null, msg: "Cadastro ok", produtoId: newProduto._id });
+    res.json({ error: null, msg: "Cadastro OK", produtoId: newProduto._id });
   } catch (error) {
     res.status(400).json({ error });
   }
 });
 
-// Restante do código permanece igual
 app.get("/cadastrousuario", async (req, res) => {
-  res.sendFile(__dirname + "/pags/cadastrousuario.html");
+  res.sendFile(__dirname + "/cadastrousuario.html");
 });
 
 app.get("/cadastroproduto", async (req, res) => {
-  res.sendFile(__dirname + "/pags/cadastroproduto.html");
+  res.sendFile(__dirname + "/cadastroproduto.html");
 });
 
 app.get("/fiaptech", async (req, res) => {
@@ -92,13 +90,9 @@ app.get("/fiaptech", async (req, res) => {
 });
 
 // Configurando a rota para arquivos estáticos (CSS, imagens, etc.)
-app.use('/static', express.static(path.join(__dirname, 'public')));
-
-
+app.use("/static", express.static(path.join(__dirname, "public")));
 
 // Iniciar o servidor
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
-
-
